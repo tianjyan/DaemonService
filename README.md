a44b331e83ce25710546e9402307d10d1ef4771f
tianjyan <youngytj@sina.com>
2017-02-12 12:17:04 +0800

    add dockfile

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# DaemonService
+# 简介
+自用的Spark Bot的样例程序
+
+# 开发准备
+* 注册[Cisco Spark](https://developer.ciscospark.com/#)开发者账号；
+* 创建[Bots](https://developer.ciscospark.com/apps.html)，创建完请记得保存到`config.json`文件中；
+* 创建[WebHook](https://developer.ciscospark.com/resource-webhooks.html)<sup>①</sup>；
+* 注册并下载[ngrok](https://ngrok.com/)，注册完成后记得保存Token；
+
+*Note*：1. WebHook的创建可以等ngrok运行后获得二级域名以后。
+
+# 运行ngrok
+    ./ngrok authtoken TOKEN // TOKEN是注册时候保存的TOKEN
+    ./ngrok http 8080 //运行完成后可获取二级运行
+
+# 真机环境中运行
+* 安装[Python 2.7.13](https://www.python.org/downloads/release/python-2713/)；
+* 安装依赖：`pip install -r requirements.txt`；
+* 运行：`python docker.py`
+
+# 阿里云环境(Ubuntu 16.04)运行
+* 安装依赖：pip install -r requirements.txt；
+* 运行：`python daemon.py start`
---
+++ docker.py
@@ -0,0 +1,33 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+# pylint: disable = C0111
+# pylint: disable = C0103
+
+import os
+import json
+from itty import run_itty
+from itty import post
+from common.logger import Logger
+from bots.chat import SparkChatBot
+
+HOST = '0.0.0.0'
+PORT = 8080
+
+@post('/spark-bot')
+def spark(request):
+    return chatBot.webhookRequest(request)
+
+logger = Logger()
+logger.info(u'Logger初始化完成')
+logger.info(u'读取配置文件')
+confileFileName = 'config.json'
+env = os.environ.get('ENV')
+if env == "DEV":
+    confileFileName = 'config.dev.json'
+configFile = file(confileFileName)
+config = json.load(configFile)
+configFile.close()
+logger.info(u'读取配置文件完成')
+chatBot = SparkChatBot(logger, config['bearer'])
+logger.info(u'启动 itty')
+run_itty(server='wsgiref', host=HOST, port=PORT)
